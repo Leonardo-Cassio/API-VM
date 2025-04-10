625c9a04d45f90e5adcf074b0b3bbc48a754828f
Your Name <fukuta2003@hotmail.com>
2025-04-10 20:23:12 -0300

    aula 10-04-2025

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Express = require('express');
 const app = Express();
+const connection = require("./database/connection");
+const bodyParser = require('body-parser');
 const porta = 4000;
 
 // engine vai ser ejs
@@ -7,6 +9,8 @@ app.set("view engine", "ejs");
 
 // pasta estatica de arquivos
 app.use(Express.static("public")); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
